62ee8e5c081c85995f34696817eec115445ad9d5
vaishaleepurohit1989-ai <vaishaleepurohit1989@gmail.com>
2026-07-27 19:54:42 +0530

    configuring app password

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'vaishaleepurohit1989@gmail.com'
-app.config['MAIL_PASSWORD'] = 'utgm ghyn dvzw cwwb'  # use Gmail App Password
+app.config['MAIL_PASSWORD'] = 'mdpukuwiqdhwkhml'  # use Gmail App Password
+app.config['MAIL_DEFAULT_SENDER'] = 'vaishaleepurohit1989@gmail.com'
+
 mail = Mail(app)
 
 @app.route('/')
@@ -24,4 +26,8 @@ def submit():
     return "Thank you! Your details were sent."
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
 
